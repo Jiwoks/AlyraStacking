639c9b9696c38ca03f717605b26389f716334c93
Teddy Blanco <tblanco@student.42quebec.com>
2022-04-30 22:53:53 -0400

    :construction: Stacking contract in progress

---
+++ contracts/Stacking_tonted.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract StackingTonted is Ownable {
+
+	/*
+	** @notice each staker has a mapping of each token staked with the amount.
+	*/
+	mapping(address => mapping(IERC20 => uint256)) stackers;
+
+	/*
+	** @notice each pool has the total ammount of token staked.
+	*/
+	mapping(IERC20 => uint256) pools;
+
+
+	function deposit(IERC20 _erc20Address, uint256 _amount) external {
+		stackers[msg.sender][_erc20Address] = _amount
+	}
+
+	function withdraw(IERC20 _erc20Address, uint256 _amount) external {
+
+	}
+
+}
--- migrations/1_stacking.js
+++ migrations/1_stacking.js
@@ -1,8 +1,9 @@
+const StackingTonted = artifacts.require("./Stacking_tonted.sol");
 const Stacking = artifacts.require("./Stacking.sol");
 const CCCToken = artifacts.require("./CCCToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(CCCToken, 100000000000);
   deployer.deploy(Stacking, 5);
-
+  deployer.deploy(StackingTonted);
 };
