dbb74774a719a989c34f91581b357c93fc9131eb
tonted <yddetco@gmail.com>
2022-05-10 22:51:59 -0400

    :bulb: add comments, to review

--- contracts/Stacking.sol
+++ contracts/Stacking.sol
@@ -13,20 +13,33 @@ contract Stacking is Ownable {
 
   IStackedERC20 rewardToken;        // Token used for rewards
 
+  /**
+   * @param oracle          : Address used for pool oracle
+   * @param balance         : Total value locked inside the pool
+   * @param rewardPerShare  : Rewards to distribute per share
+   * @param rewardPerSecond : Rewards to distribute per second
+   * @param lastRewardBlock : Last block timestamp where rewards are evaluated
+   */
   struct Pool {
-    address oracle;          // Address used for pool oracle
-    uint256 balance;         // Total value locked inside the pool
-    uint256 rewardPerShare;  // Rewards to distribute per share
-    uint256 rewardPerSecond; // Rewards to distribute per second
-    uint256 lastRewardBlock; // Last block timestamp where rewards are evaluated
+    address oracle;
+    uint256 balance;
+    uint256 rewardPerShare;
+    uint256 rewardPerSecond;
+    uint256 lastRewardBlock;
   }
 
+  /**
+   * @param balance   : Amount of token provided by this account
+   * @param rewardDebt: Reward debt amount
+   */
   struct Account {
     uint256 balance;         // Amount of token provided by this account
     uint256 rewardDebt;      // Reward debt amount
   }
 
   mapping (IERC20 => Pool ) public pools;
+
+  // @dev [msg.sender][token] = {balance, rewardDebt}
   mapping (address => mapping (IERC20 => Account)) public accounts;
 
   event PoolCreated (IERC20 token, address oracle, string symbol);
@@ -34,6 +47,11 @@ contract Stacking is Ownable {
   event Withdraw (IERC20 token, address account, uint256 amount);
   event Claim (address account, uint256 amount);
 
+  /**
+   * @notice Check if the token pool exists
+   *
+   * @param _token  : token address to check
+   */
   modifier onlyCreatedToken (IERC20 _token) {
     require(pools[_token].oracle != address(0), 'Token not yet allowed');
     _;
@@ -43,6 +61,20 @@ contract Stacking is Ownable {
     rewardToken = _rewardToken;
   }
 
+  /**
+   * @notice Creation of a pool to allow users to stake their token
+   * @notice The value of the token is peg with de Chainlink oracle
+   * @notice Only the owner can create a pool
+   *
+   * @param _token          : address of the token put in pool
+   * @param _oracle         : address of the Chainlink oracle for this token
+   * @param _rewardPerSecond: reward per second for this pool
+   * @param symbol          : symbol of the token
+   *
+   * @emits PoolCreated (_token, _oracle, symbol).
+   */
+  // TODO remove symbol and use _token.symbol()
+  // TODO BONUS function for create a pool with anything token and peg then to default oracle
   function createPool (IERC20 _token, address _oracle, uint256 _rewardPerSecond, string calldata symbol) onlyOwner external {
     require (pools[_token].oracle == address(0), 'Token already attached');
     pools[_token].oracle = _oracle;
@@ -51,6 +83,11 @@ contract Stacking is Ownable {
     emit PoolCreated (_token, _oracle, symbol);
   }
 
+  /**
+   * @notice Update the data of the pool
+   *
+   * @param _token          : address of the token where the pool must be update
+   */
   function _updatePool (IERC20 _token) internal {
     Pool storage pool = pools[_token];
     uint256 currentRewardBlock = block.timestamp;
@@ -65,6 +102,16 @@ contract Stacking is Ownable {
     pool.lastRewardBlock = currentRewardBlock;
   }
 
+  /**
+   * @notice Deposit token to stake by the user
+   * @notice Update the data of the pool
+   * @notice Send current rewards if exists to the sender
+   *
+   * @param _token  : token address to stake
+   * @param _amount : deposit amount
+   *
+   * @emits Deposit (_token, msg.sender, _amount);
+   */
   function deposit (IERC20 _token, uint256 _amount) onlyCreatedToken (_token) external {
     require(_amount > 0, 'Only not null amount');
 
@@ -87,7 +134,17 @@ contract Stacking is Ownable {
     emit Deposit (_token, msg.sender, _amount);
   }
 
-  function withdraw (IERC20 _token, uint256 _amount) onlyCreatedToken (_token) external {
+  /**
+   * @notice Withdraw the token staken by the user
+   * @notice Update the data of the pool
+   * @notice Send current rewards pending
+   *
+   * @param _token  : token address to unstake
+   * @param _amount : deposit amount
+   *
+   * @emits Deposit (_token, msg.sender, _amount);
+   */
+  function Withdraw (IERC20 _token, uint256 _amount) onlyCreatedToken (_token) external {
     require(accounts[msg.sender][_token].balance > 0 && _amount <= accounts[msg.sender][_token].balance, 'Insufficient balance');
     require(_amount > 0, "Amount 0");
 
@@ -112,20 +169,55 @@ contract Stacking is Ownable {
     emit Withdraw (_token, msg.sender, _amount);
   }
 
+  /**
+   * @notice
+   *
+   * @param _token  : address of the token to know the balance
+   *
+   * @return the total amount of the tokens staked in the pool by the sender
+   */
+  // TODO named the returns variable
   function balanceOf (IERC20 _token) onlyCreatedToken(_token) external view returns (uint256) {
     return accounts[msg.sender][_token].balance;
   }
 
+
+  /**
+   * @notice
+   *
+   * @param _token  : address of the token to know the balance
+   *
+   * @return the total amount of the tokens staked in the pool
+   */
+  // TODO named the returns variable
   function tvlOf (IERC20 _token) external view returns (uint256) {
     return pools[_token].balance;
   }
 
+  /**
+   * @notice mint the token needed for send using the mint methods of the ERC20.
+   * @notice send the reward using the safeTransfer methode of SafeERC20.
+   *
+   * @param _to     : address to send the reward
+   * @param _amount : amount of reward to send
+   *
+   */
+  // TODO emit the claim event here instead withdraw and deposit function
   function safeRewardTransfer(address _to, uint256 _amount) internal {
     rewardToken.mint(_amount);
     rewardToken.safeTransfer(_to, _amount);
   }
 
   // View function to see pending Tokens on frontend.
+  /**
+   * @dev function to see pending Tokens on frontend.
+   *
+   * @param _token  : token address of the pool to check the pending reward
+   * @param _user   : account address of the user to check the pending reward
+   *
+   * @return the pending reward
+   */
+  // TODO named the returns variable
   function claimable(IERC20 _token, address _user) external view returns (uint256 rewards) {
     Pool memory pool = pools[_token];
     Account memory account = accounts[_user][_token];
