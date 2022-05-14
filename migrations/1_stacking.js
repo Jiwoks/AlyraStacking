912902e97043a3f1649d1457b396081a86258e08
damien <damien.barrere@gmail.com>
2022-05-14 23:08:25 +0200

    Improve migration

--- migrations/1_stacking.js
+++ migrations/1_stacking.js
@@ -25,32 +25,15 @@ module.exports = async (deployer, network, account) => {
   const usdt = await Usdt.deployed();
   await deployer.deploy(Stacking, myERC20.address);
   const myStacking = await Stacking.deployed();
-  await deployer.deploy(MockOracleDAI, '18', '1', 'Mock Oracle DAI');
-  const myMockOracleDAI = await MockOracleDAI.deployed();
-  await deployer.deploy(MockOracleDAI, '18', '1', 'Mock Oracle DAI');
-  const myMockOracleLINK = await MockOracleLINK.deployed();
 
+  if (network === 'development') {
+    await deployer.deploy(MockOracleDAI, '18', '1', 'Mock Oracle DAI');
+    const myMockOracleDAI = await MockOracleDAI.deployed();
+    await deployer.deploy(MockOracleDAI, '18', '1', 'Mock Oracle DAI');
+    const myMockOracleLINK = await MockOracleLINK.deployed();
+  }
   // Allow stacking contract to mint CCC Token
   await myERC20.allowAdmin(myStacking.address);
-
-  const aggregators = require(path.join(__dirname, '..', 'dataFeed', 'chainlink.json'));
-  const contracts = require(path.join(__dirname, '..', 'dataFeed', 'contracts.json'));
-
-  if (aggregators?.[process.env.NETWORK_NAME]?.[process.env.CCC_PEG]) {
-    for (const tokenSymbol of Object.getOwnPropertyNames(aggregators[process.env.NETWORK_NAME][process.env.CCC_PEG])) {
-      try {
-        if (contracts[process.env.NETWORK_NAME][tokenSymbol] === undefined) {
-          continue;
-        }
-
-        console.log('Adding token ' + tokenSymbol + ' ' + aggregators[process.env.NETWORK_NAME][process.env.CCC_PEG][tokenSymbol] + ' ' + contracts[process.env.NETWORK_NAME][tokenSymbol]);
-
-        await myStacking.createPool(contracts[process.env.NETWORK_NAME][tokenSymbol], aggregators[process.env.NETWORK_NAME][process.env.CCC_PEG][tokenSymbol], tokenSymbol);
-      } catch (e) {
-        console.warn(e);
-      }
-    }
-  }
 
   if (network === 'development') {
     await myMockOracleDAI.setData('100376540');
@@ -58,8 +41,8 @@ module.exports = async (deployer, network, account) => {
     await myMockOracleLINK.setData('707782127');
     await myStacking.createPool(link.address, myMockOracleLINK.address, '8', web3.utils.toWei('5'), 'LINK');
   } else if (network === 'kovan' || network === 'kovan') {
-    await myStacking.createPool(dai.address, '0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541', 12, web3.utils.toWei('5'), 'DAI');
-    await myStacking.createPool(link.address, '0x3Af8C569ab77af5230596Acf0E8c2F9351d24C38', 12, web3.utils.toWei('5'), 'LINK');
+    await myStacking.createPool(dai.address, '0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541', 8, web3.utils.toWei('5'), 'DAI');
+    await myStacking.createPool(link.address, '0x3Af8C569ab77af5230596Acf0E8c2F9351d24C38', 8, web3.utils.toWei('5'), 'LINK');
   }
 
 };
