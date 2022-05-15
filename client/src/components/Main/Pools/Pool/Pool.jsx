d8d0540a0cc197542ae46ad220fad74e5c5579ad
damien <damien.barrere@gmail.com>
2022-05-15 09:07:46 +0200

    Update pool data when wallet disconnected

--- client/src/components/Main/Pools/Pool/Pool.jsx
+++ client/src/components/Main/Pools/Pool/Pool.jsx
@@ -87,6 +87,9 @@ function Pool({pool, ...props}) {
 
     useEffect(() => {
         if (!walletAddress) {
+            setWalletAmount('-');
+            setDepositedAmount('-');
+            setValueClaimable(0);
             return;
         }
 
