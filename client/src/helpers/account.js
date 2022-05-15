489aaa3b4e3af6de6c3bc1bfb20aa6f77ac2c011
damien <damien.barrere@gmail.com>
2022-05-15 17:14:17 +0200

    Fix account not loading

--- client/src/helpers/account.js
+++ client/src/helpers/account.js
@@ -1,7 +1,7 @@
 import web3 from "./web3";
 
 const getAccount = async () => {
-    const accounts = await (await web3).eth.getAccounts();
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
     return accounts[0];
 }
 
